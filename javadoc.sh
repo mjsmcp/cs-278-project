23295671c3d688d99e375d0138b659711324bf70
Syddraf Cray <syddraf@lastorder.(none)>
2011-12-01 15:01:16 -0600

    Added javadoc pdf generation capabilities. Will only run on matthew's machine.

---
+++ javadoc.sh
@@ -0,0 +1,2 @@
+javadoc -doclet com.aurigalogic.doclet.core.Doclet -docletpath /home/syddraf/Downloads/aurigadoclet/bin/AurigaDoclet.jar src/edu/vu/vuse/cs278/g3/engine/*.java src/edu/vu/vuse/cs278/g3/model/*.java src/edu/vu/vuse/cs278/g3/gui/*.java -out doc/javadoc.pdf -format pdf
+
