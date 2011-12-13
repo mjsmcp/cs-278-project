aa41da303f4dc1786510b6ba6591946be2ea8663
Yi-Chin Sun <yichin.sun@gmail.com>
2011-12-12 21:41:47 -0600

    Added space

--- src/edu/vu/vuse/cs278/g3/gui/MainWindow.java
+++ src/edu/vu/vuse/cs278/g3/gui/MainWindow.java
@@ -588,7 +588,7 @@ private void stopSimulationActionPerformed(java.awt.event.ActionEvent evt) {//GE
 	@Override
 	public void cycleComplete() {
 		int speed = (int)(ObjectManager.getInstance().getObject("bus").getSpeed())/8;
-		busSpeed.setText(Integer.toString(speed) + "mph");
+		busSpeed.setText(Integer.toString(speed) + " MPH");
 	}
 	
 }
